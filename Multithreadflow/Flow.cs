c2559fbd768138885395c5fcb89548a386ac866d
Philip Doxakis <philip@doxakis.com>
2017-06-04 23:52:43 -0400

    feat: proof of concept working

---
+++ Multithreadflow/Flow.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreadflow
+{
+    public class StepConfig
+    {
+        public bool ModeContinue { get; set; }
+        public int CheckInterval { get; set; }
+        public int DurationModeContinue { get; set; }
+        public Action<int> Update { get; set; }
+        
+        public int NumThreadStopped { get; set; }
+        public Action<int, List<object>> Action { get; set; }
+        public int[] MaxItemsPerThread { get; set; }
+        
+        public List<object> Queue { get; set; } = new List<object>();
+    }
+
+    public class FlowConfig
+    {
+        public List<StepConfig> Step = new List<StepConfig>();
+    }
+
+    public class ThreadParameters
+    {
+        public int ThreadId { get; set; }
+        public StepConfig Step { get; set; }
+    }
+
+    public class Flow
+    {
+        public FlowConfig Config { get; set; }
+        private List<Thread> listThread = new List<Thread>();
+
+        private object lockThread = new object();
+        public void Start()
+        {
+            foreach (var stepItem in Config.Step)
+            {
+                for (int i = 0; i < stepItem.MaxItemsPerThread.Length; i++)
+                {
+                    Thread t = new Thread(new ParameterizedThreadStart((parameters) =>
+                    {
+                        var tParam = parameters as ThreadParameters;
+                        var step = tParam.Step;
+                        int stepIndex = Config.Step.IndexOf(step);
+                        int ticks = 0;
+
+                        while (Thread.CurrentThread.IsAlive)
+                        {
+                            // Vérifier si le thread est tjrs en vie.
+                            bool checkIsAlive = true;
+                            if (stepIndex > 0)
+                            {
+                                var previousStep = Config.Step[stepIndex - 1];
+                                if (previousStep.NumThreadStopped != previousStep.MaxItemsPerThread.Length)
+                                {
+                                    checkIsAlive = false;
+                                }
+                            }
+                            if (checkIsAlive)
+                            {
+                                if (!step.Queue.Any() && !step.ModeContinue)
+                                {
+                                    step.NumThreadStopped++;
+                                    break;
+                                }
+                            }
+
+                            // Vérifier s'il y a de la job à faire.
+                            List<object> queueThread = new List<object>();
+                            lock (lockThread)
+                            {
+                                int maxItems = step.MaxItemsPerThread[tParam.ThreadId];
+
+                                var count = step.Queue.Count;
+                                if (count > maxItems
+                                    ||
+                                    ticks % 40 == 0 && count > 0 /* Attendre plus longtemps s'il n'y a pas suffisamment d'élément */
+                                    )
+                                {
+                                    // Il y a suffisamment d'élément ou on a attendu un peu.
+                                    queueThread.AddRange(step.Queue.Take(maxItems));
+                                    foreach (var item in queueThread)
+                                    {
+                                        step.Queue.Remove(item);
+                                    }
+                                }
+                            }
+
+                            if (queueThread.Any())
+                            {
+                                step.Action(tParam.ThreadId, queueThread);
+                            }
+
+                            // Mode continue: update.
+                            if (step.ModeContinue && ticks % step.CheckInterval == 0)
+                            {
+                                step.Update(tParam.ThreadId);
+                            }
+
+                            // Mode continue: verifier si l'on a atteint la durée maximal.
+                            if (step.ModeContinue && step.DurationModeContinue <= ticks)
+                            {
+                                step.ModeContinue = false;
+                            }
+
+                            ticks++;
+                            Thread.Sleep(10);
+                        }
+                    }));
+                    t.Start(new ThreadParameters
+                    {
+                        Step = stepItem,
+                        ThreadId = i
+                    });
+                    listThread.Add(t);
+                }
+            }
+        }
+
+        public void Join()
+        {
+            listThread.ForEach(m => m.Join());
+        }
+    }
+}
---
+++ Multithreadflow/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreadflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 50; n < 100; n++)
+            {
+                List<object> queue = new List<object>();
+                for (int j = 0; j < n; j++)
+                {
+                    queue.Add(j);
+                }
+
+                Flow m = new Flow();
+                m.Config = new FlowConfig();
+                
+                m.Config.Step.Add(new StepConfig
+                {
+                    Queue = queue,
+                    MaxItemsPerThread = new int[] { 1 },
+                    Action = (threadId, items) =>
+                    {
+                        var i = (int)items.First();
+
+                        Thread.Sleep(i % 10 + 50);
+                        Console.WriteLine(" Thread : " + threadId + " Step 0 : " + i);
+
+                        m.Config.Step[1].Queue.AddRange(items);
+                    },
+                    ModeContinue = true,
+                    CheckInterval = 200,
+                    DurationModeContinue = 600,
+                    Update = (threadId) =>
+                    {
+                        int nouvelItem = 1000;
+
+                        m.Config.Step[1].Queue.Add(nouvelItem);
+
+                        Console.WriteLine(" Thread : " + threadId + " Update");
+                    }
+                });
+                
+                m.Config.Step.Add(new StepConfig
+                {
+                    MaxItemsPerThread = new int[] { 1, 1, 1, 1 },
+                    Action = (threadId, items) =>
+                    {
+                        var i = (int)items.First();
+
+                        Thread.Sleep(i % 10 + 500);
+                        Console.WriteLine(" Thread : " + threadId + " Step 1 : " + i);
+
+                        // Supposons qu'on va créer quelques items à partir d'un item.
+                        for (int k = 0; k < 5; k++)
+                        {
+                            m.Config.Step[2].Queue.Add(i+ "_" + k);
+                        }
+                    }
+                });
+                
+                m.Config.Step.Add(new StepConfig
+                {
+                    MaxItemsPerThread = new int[] { 64, 16, 16 },
+                    Action = (threadId, items) =>
+                    {
+                        var ids = string.Join(", ", items);
+
+                        Thread.Sleep(items.Count * 10 + 1500);
+                        Console.WriteLine(" Thread : " + threadId + " Step 2 : (" + items.Count + ") " + ids);
+
+                        m.Config.Step[3].Queue.AddRange(items);
+                    }
+                });
+                
+                m.Config.Step.Add(new StepConfig
+                {
+                    MaxItemsPerThread = new int[] { 1 },
+                    Action = (threadId, items) =>
+                    {
+                        var i = (string)items.First();
+
+                        Thread.Sleep(20);
+                        Console.WriteLine(" Thread : " + threadId + " Step 3 : " + i);
+                    }
+                });
+
+                Console.WriteLine("Starting...");
+
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                m.Start();
+                m.Join();
+
+                stopwatch.Stop();
+
+                Console.WriteLine("n = " + n + ", duration = " + stopwatch.ElapsedMilliseconds + " ms");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
